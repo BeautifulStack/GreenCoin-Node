7a40ea2f480c1f05bab708fc8b4038987154a0e0
sven-eliasen <jo.pettersen@pm.me>
2021-05-29 19:07:26 +0200

    AD: check request body

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from utils.keys import KeyPair
 from utils.blockchain import Blockchain
 
 app = Flask(__name__)
-
 keys = KeyPair()
 blockchain = Blockchain()
 
@@ -13,9 +12,18 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/chain', methods=['GET'])
+@app.get('/chain')
 def get_chain():
     return blockchain.get_chain(), 200, {'Content-Type': 'application/json'}
+
+
+@app.post('/new_transaction')
+def new_transaction():
+    """
+    Takes form-data encoding for now, may switch to JSON later
+    """
+
+    return blockchain.new_transaction(request.form)
 
 
 if __name__ == '__main__':
--- utils/blockchain.py
+++ utils/blockchain.py
@@ -5,10 +5,10 @@ from os import path
 
 
 class Blockchain:
-    length = 0
-    chain = []
-    open_transaction = []
-    last_block_hash = None
+    __length = 0
+    __chain = []  # el famoso blockchain
+    __open_transaction = []
+    __last_block_hash = None
 
     def __init__(self):
         if not path.exists("data/chain.json"):
@@ -18,9 +18,29 @@ class Blockchain:
         with open("data/chain.json", "r") as f:
             chain = json.load(f)
 
-        self.length = chain["length"]
-        self.chain = chain["chain"]
-        self.last_block_hash = base64.b64encode(hashlib.sha256(json.dumps(self.chain[0]).encode()).digest()).decode()
+        self.__length = chain["length"]
+        self.__chain = chain["chain"]
+        self.__last_block_hash = base64.b64encode(
+            hashlib.sha256(json.dumps(self.__chain[0]).encode()).digest()).decode()
 
     def get_chain(self):
-        return json.dumps({'length': self.length, 'chain': self.chain})
+        return json.dumps({'length': self.__length, 'chain': self.__chain})
+
+    def new_transaction(self, body):
+        if not self.__check_body_transaction(body):
+            return {"error": "Missing argument(s)"}, 400, {'Content-Type': 'application/json'}
+
+    def __very_authenticity(self, sender, public_key):
+        pass
+
+    @staticmethod
+    def __check_body_transaction(body):
+        condition = "transaction" in body or "signature" in body or "public_key" in body
+        if condition:
+            condition = "sender" in body or "receiver" in body or "public_key" in body or "time" in body
+            if condition:
+                return True
+            else:
+                return False
+        else:
+            return False
