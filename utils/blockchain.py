d5592bd3a7c56aeb5c6ab0812460343a74886b10
sven-eliasen <jo.pettersen@pm.me>
2021-06-05 23:55:08 +0200

    debug again

--- utils/blockchain.py
+++ utils/blockchain.py
@@ -33,11 +33,13 @@ class Blockchain:
                 self.__tx_length = self.__chain[-1]["transactions"][-1]["index"]
                 self.__last_block_hash = sha256(json.dumps(self.__chain[-1]))
 
-                print(self.__last_block_hash)
+                print(json.dumps(self.__chain[-1]))
         else:
             if not self.__load_chain():
                 print("ERR: Couldn't find chain data, copy chain-sample.json to chain.json", file=sys.stderr)
                 exit(1)
+
+            print(json.dumps(self.__chain[-1]))
 
             miner = Thread(target=self.__mine, daemon=True)
             miner.start()
